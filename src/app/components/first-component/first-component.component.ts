3eb8ad988a54d407d762f03e31cdf1b99290b38e
Gustavo Alencar <gustavorique@live.com>
2023-10-24 03:14:28 -0300

    Curso Angular 13: Dados no template - #05

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit {
+
+  name: string = "Gustavo"
+  age: number = 31
+  hobbies = ['Jogar', 'Programar', 'Viajar']
+
+  car = {
+    name: "New Fiesta",
+    year: 2013
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+      
+  }
 
 }
