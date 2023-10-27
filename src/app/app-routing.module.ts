026d71d58febbaa3aae6dccb35c291e1ce9e7c50
Gustavo Alencar <gustavorique@live.com>
2023-10-27 00:47:51 -0300

    Criado um exemplo prática da aula 17 para exercício

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { ListRenderComponent } from "./components/list-render/list-render.component";
+import { EmiterComponent } from "./components/emiter/emiter.component";
 
 const routes: Routes = [
     {path: '', component: FirstComponentComponent},
-    {path: 'list', component: ListRenderComponent}
+    {path: 'list', component: ListRenderComponent},
+    {path: 'change', component: EmiterComponent}
 ]
 
 @NgModule({
